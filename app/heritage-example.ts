44d98a1c51bdf7c15eb45d4d6130c8547cc0c396
itc-n24009 <n24009@std.it-college.ac.jp>
2025-02-25 07:10:29 +0000

    世界遺産の例の表示

---
+++ app/heritage-example.ts
@@ -0,0 +1,53 @@
+export const heritageSites = [
+  {
+    place_id: "1",
+    name: "富士山",
+    geometry: { location: { lat: 35.3606, lng: 138.7274 } },
+  },
+
+  {
+    place_id: "2",
+    name: "グレート・バリア・リーフ",
+    geometry: { location: { lat: -18.2871, lng: 147.6992 } },
+  },
+  {
+    place_id: "3",
+    name: "マチュ・ピチュ",
+    geometry: { location: { lat: -13.1631, lng: -72.545 } },
+  },
+  {
+    place_id: "4",
+    name: "エッフェル塔",
+    geometry: { location: { lat: 48.8584, lng: 2.2945 } },
+  },
+  {
+    place_id: "5",
+    name: "ピラミッド群",
+    geometry: { location: { lat: 29.9792, lng: 31.1342 } },
+  },
+  {
+    place_id: "6",
+    name: "自由の女神",
+    geometry: { location: { lat: 40.6892, lng: -74.0445 } },
+  },
+  {
+    place_id: "7",
+    name: "ストーンヘンジ",
+    geometry: { location: { lat: 51.1789, lng: -1.8262 } },
+  },
+  {
+    place_id: "8",
+    name: "タージ・マハル",
+    geometry: { location: { lat: 27.1751, lng: 78.0421 } },
+  },
+  {
+    place_id: "9",
+    name: "イグアスの滝",
+    geometry: { location: { lat: -25.6953, lng: -54.4367 } },
+  },
+  {
+    place_id: "10",
+    name: "アンコール・ワット",
+    geometry: { location: { lat: 13.4125, lng: 103.8667 } },
+  },
+];
